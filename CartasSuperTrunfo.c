736f88e11a69911e17ff004988ed2597ed7d618e
Cláudio Eduardo Martins Machado <martinsclaudioeduardo0@gmail.com>
2025-09-22 21:09:11 +0000

    Jogo Super Trunfo 2

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -8,20 +8,20 @@ int main (){
  char Estado1[]="RS";
  char codigo_carta1[] = "A01";
  char cidade1[] = "Porto Alegre";
-
-float  populacao1 = 1388.794f;
+ float  populacao1 = 1388.794f;
  float area1 = 496.8f; 
  float pib = 81.56f;
  int pontos_turisticos1 = 38;
 
  //Segunda Carta 
  char estado2[] = "RJ";
- char codigo_carta1[] = "B02"; ;
+ char codigo_carta1[] = "B02"; 
  char cidade2[] = "Rio de Janeiro";
  float populacao2 = 179.120f;
  float area2 = 813.420f;
  float pib2 = 11.194f;
  int pontos_turisticos = 10;  
+ 
  return 0;
 
  
