861cdc6d4b82918e06f16a7ea9d97a0932b78ffa
Cláudio Eduardo Martins Machado <martinsclaudioeduardo0@gmail.com>
2025-09-22 20:41:46 +0000

    porto alegre/angra dos reis

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,14 +4,26 @@ int main (){
 
  printf("desafio cartas super trunfo!\n");
 
- char Estado;
- char carta;
- char cidade;
- int populacao;
- float area;
- float PIB;
- int turisticos;
+ char Estado = 'A';
+ char codigo_carta = 'A01';
+ char cidade = 'Porto Alegre';
+ int populacao = 1388.794;
+ float area = 496.8; 
+ float pib = 81.56;
+ int pontos_turisticos = 38;
+
+ char estado = 'B';
+ char codigo_carta = 'B02';
+ char cidade = 'angra dos reis';
+ int populacao = 179.120;
+ float area = 813.420;
+ float pib = 11.194;
+ int pontos_turisticos = 10; 
+
  
+
+
+
 
    
 
