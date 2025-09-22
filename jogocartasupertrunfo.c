f60ef111f06d9f4549515bb3f5d0f0d7336f418c
Cláudio Eduardo Martins Machado <martinsclaudioeduardo0@gmail.com>
2025-09-22 21:22:36 +0000

    Jogo

---
+++ jogocartasupertrunfo.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main (){
+
+    printf("desafio cartas super trunfo!\n");
+    
+    // Primeira carta
+    char Estado1[]="RS";
+    char codigo_carta1[] = "A01";
+    char cidade1[] = "Porto Alegre";
+    float  populacao1 = 1388.794f;
+    float area1 = 496.8f; 
+    float pib = 81.56f;
+    int pontos_turisticos1 = 38;
+    
+    //Segunda Carta 
+    char estado2[] = "RJ";
+    char codigo_carta1[] = "B02"; 
+    char cidade2[] = "Rio de Janeiro";
+    float populacao2 = 179.120f;
+    float area2 = 813.420f;
+    float pib2 = 11.194f;
+    int pontos_turisticos = 10;  
+    
+    return 0;
+}
