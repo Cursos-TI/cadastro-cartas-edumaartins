343f7a8f16fda0029d859c781ee1e60b4bd3ca05
Cláudio Eduardo Martins Machado <martinsclaudioeduardo0@gmail.com>
2025-09-28 19:30:07 +0000

    Prática De Matrizes

---
+++ #include <stdio.c
@@ -0,0 +1,14 @@
+#include  <stdio.h>
+
+int main() { 
+
+int index;
+
+char * nomesAlunos = { 
+    {"Aluno 1", "Pt: 30, "Mat: 90"},
+    {"Aluno 2", "Pt: 60, "Mat: 60"},
+    {"Aluno 3", "Pt: 90", "Mat: 30"},
+}
+
+    return 0;
+}
