7e7af5d634d45d321f674c8f6a9e113cb0b696e3
Cláudio Eduardo Martins Machado <martinsclaudioeduardo0@gmail.com>
2025-09-30 22:56:47 +0000

    Aprendendo Variaveis

--- n1_nc1_1.c
+++ n1_nc1_1.c
@@ -7,10 +7,23 @@ int main(){
     float altura = 1.75;
     double peso = 90.3;
     char nome [20] = 'Eduardo';
-    char letra = 'A'; 
-idade = 25;
+    char letra = 'E'; 
 
-quantidade = 10;
+    printf("A idade do %s é: %d\n", nome, idade);
+    printf("A altura é: %e\n", altura);
+    printf("O peso é: %f\n", peso);
+    printf("A quantidade é: %d\n", quantidade);
+    printf("A inicial do nome é: %c\n", letra);
+    /*
+    printf("%formato1 %formato2 %formato3 , variavel1, variavel2, variavel3)
+    
+    %d: Imprime um inteiro no formato decimal.
+    %i: Equivalente a%d.
+    %f: Imprime um numero de ponto flutuante no formato padrão.
+    %e: Imprime um numero de ponto flutuante na notação cientifica.
+    %c: Imprime um unico caractere.
+    %s: Imprime uma cadeia  (String)") de caracteres.
+    */
     
     
 
